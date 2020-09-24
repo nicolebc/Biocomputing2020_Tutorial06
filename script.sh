b0e4d39242de6d1c8ab77f0dbf8e4c6ff912168b
nicolebc <ncheng@nd.edu>
2020-09-23 21:27:19 -0400

    asf

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
-cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -k2
+#Task 1
+cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -V -k2
 
-# Step 2
+#Task 2
 echo "Lowest Earning Wages"
-cat wages.csv | cut -d "," -f 1,2,4 | tr "," " " | sort -k3 | head -1
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -V -k3 | head -1
 echo "Highest Earning Wages"
-cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -k3 | tail -2 | head -1
+cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -V -k3 | tail -2 | head -1
 echo "Number of Females top 10 wages"
-cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -k3 | tail -11 | grep -E -c "female"
+cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -V -k3 | tail -11 | grep -E -c "female"
+
+#Task 3
+
