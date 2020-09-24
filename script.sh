55227d1b50181cf7f5a57233045980981328f445
began4 <began4@nd.edu>
2020-09-23 21:13:37 -0400

    update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,9 @@
 cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -k2
 
+# Step 2
+echo "Lowest Earning Wages"
+cat wages.csv | cut -d "," -f 1,2,4 | tr "," " " | sort -k3 | head -1
+echo "Highest Earning Wages"
+cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -k3 | tail -2 | head -1
+echo "Number of Females top 10 wages"
+cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -k3 | tail -11 | grep -E -c "female"
