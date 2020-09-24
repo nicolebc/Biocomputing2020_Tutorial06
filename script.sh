76ff262fdc4b96e41ffed2608f58cc0fdfdacb54
Samantha Masca <samanthamasca@Samanthas-MacBook-Air.local>
2020-09-23 21:57:09 -0400

    finalized task

--- script.sh
+++ script.sh
@@ -10,4 +10,9 @@ echo "Number of Females top 10 wages"
 cut -d "," -f 1,2,4 wages.csv | tr "," " " | sort -V -k3 | tail -11 | grep -E -c "female"
 
 #Task 3
+wages16=$(cat wages.csv | cut -d , -f 3,4 | sort -V | grep -w "^16" | tail -n 1 | cut -d , -f 2) 
 
+wages12=$(cat wages.csv | cut -d , -f 3,4 | sort -V | grep -w "^12" | tail -n 1 | cut -d , -f 2)
+
+echo "The difference in wage is" 
+echo "$wages16 - $wages12" | bc
